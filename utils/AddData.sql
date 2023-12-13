279abbb160663246ca03358772902d17319dbba4
xinyu lu <xinyulu@stu.xmu.edu.cn>
2023-12-13 16:42:14 +0800

    init SQL file

---
+++ utils/AddData.sql
@@ -0,0 +1,28 @@
+INSERT INTO
+    spectra_database (
+        ds,
+        raw_wavenumber,
+        raw_spectrum,
+        pre_spectrum,
+        wave_range,
+        if_smooth,
+        smooth_method,
+        smooth_args,
+        if_baseline,
+        baseline_method,
+        baseline_args
+    )
+VALUES
+    (
+        "{}",
+        "{}", 
+        "{}", 
+        "{}", 
+        "{}", 
+        {},   
+        "{}", 
+        "{}", 
+        {},   
+        "{}", 
+        "{}"  
+    );
