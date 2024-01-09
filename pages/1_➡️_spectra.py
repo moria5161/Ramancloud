3931e87c7eef70d61acba116c9f38b96906d7ada
xinyu lu <xinyulu@stu.xmu.edu.cn>
2024-01-09 09:38:20 +0800

    fix bugs about loading data

--- pages/1_➡️_spectra.py
+++ pages/1_➡️_spectra.py
@@ -52,8 +52,11 @@ def load_data(file):
 
     # load string with delimiter '\t' and ',' automaticlly to DataFrame
     spec = pd.read_csv(io.BytesIO(content), delimiter=delimiter, header=None)
-    # delete the columns without numbers
-    res = pd.DataFrame({'wavenumber':spec.iloc[:, 0], 'raw':spec.iloc[:, -1]})
+    
+    if len(spec.columns) >= 4:
+        res = pd.DataFrame({'wavenumber':spec.iloc[:, -2], 'raw':spec.iloc[:, -1]})
+    else:
+        res = pd.DataFrame({'wavenumber':spec.iloc[:, 0], 'raw':spec.iloc[:, -1]})
     # spec.columns = []
     st.session_state['raw_spec'] = res
     return res 
