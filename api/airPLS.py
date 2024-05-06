b8aaf2402b59e3f260d984037f0932713abfd567
xinyu lu <xinyulu@stu.xmu.edu.cn>
2024-05-06 10:34:49 +0800

    fix airpls bug

--- api/airPLS.py
+++ api/airPLS.py
@@ -51,7 +51,7 @@ def ZhangFit(x, lambda_=100, porder=1, repitition=15):
         corrected=WhittakerSmooth(yorig,w,lambda_, porder)
         d=yorig-corrected
         dssn=np.abs(d[d<0].sum())
-        if(dssn<0.001*(abs(yorig)).sum() or i==repitition):
+        if(dssn<=0.001*(abs(yorig)).sum() or i==repitition):
             # if(i==repitition): print('WARING max iteration reached!')
             break
         w[d>=0]=0 # d>0 means that this point is part of a peak, so its weight is set to 0 in order to ignore it
