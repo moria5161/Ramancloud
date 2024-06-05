cd2603ad174a5bc2d439c290caad6c6082123f4b
xinyu lu <xinyulu@stu.xmu.edu.cn>
2024-06-05 16:08:54 +0800

    add Nanophoton setting

--- pages/5_🛠️_other_tools.py
+++ pages/5_🛠️_other_tools.py
@@ -53,7 +53,7 @@ elif mode == "split mapping into spectra":
     # select the instrument
     instrument = st.radio(
     "Which instrument are these data from?",
-    ["**select one**:point_right:", "Renishaw", "Horiba"],
+    ["**select one**:point_right:", "Renishaw", "Horiba", "Nanophoton"],
     horizontal=True,)
 
     if instrument == "**select one**:point_right:": st.stop()
@@ -61,6 +61,7 @@ elif mode == "split mapping into spectra":
     upload = st.file_uploader("Upload a file", type="txt")
     if upload is not None:
         st.warning('upload success')
+        
         if instrument == "Horiba":
             df = pd.read_csv(upload, delimiter='\t', header=None)
             filename = upload.name
@@ -79,6 +80,30 @@ elif mode == "split mapping into spectra":
             data = df.iloc[:, -1].to_numpy().reshape(batch, -1)
             
             files = [np.c_[wave[i], data[i]] for i in range(batch)]
+        
+        elif instrument == "Nanophoton":
+            filename = upload.name
+            content = upload.read().decode("utf-8")
+            data = content.splitlines()
+            list = data[2].split('\t')
+            arr_w, arr_s = [], []
+            
+            for i in range(1, len(data)):
+                wavenum = []
+                signal = []
+                for l in data[i].split('\t'):
+                    if '.' not in l:
+                        signal.append(int(l))
+                    else:
+                        wavenum.append(float(l))
+                arr_w.append(np.array(wavenum))
+                arr_s.append(np.array(signal))
+            
+            arr_w = np.stack(arr_w)
+            arr_s = np.stack(arr_s)
+            
+            # 存储结果
+            files = [np.c_[arr_w[:, i], arr_s[:, i]] for i in range(1, arr_w.shape[1])]
 
 
         with io.BytesIO() as zip_buffer: # Create an in-memory zip file
@@ -87,10 +112,10 @@ elif mode == "split mapping into spectra":
                     # Convert the ndarray to bytes
                     arr_bytes = io.BytesIO()
                     # Create an in-memory file-like object for each array
-                    np.savetxt(arr_bytes, arr, delimiter=',', fmt='%s')
+                    np.savetxt(arr_bytes, arr, delimiter='\t', fmt='%s')
                     arr_bytes.seek(0)
                     # Add the in-memory file to the zip file
-                    zip_file.writestr(f'{filename}_split_{i+1}.txt', arr_bytes.getvalue())
+                    zip_file.writestr(f'{filename[:-4]}_split_{i+1}.txt', arr_bytes.getvalue())
                 print(zip_file)
             href = generate_download_link(zip_buffer, 'split.zip')
             
