0a28a35142bf3f262953572c7a6466ae6a03a896
xinyu lu <xinyulu@stu.xmu.edu.cn>
2024-06-07 11:06:03 +0800

    add modules to draw molecules

--- pages/4_✔️_structure_to_spectrum.py
+++ pages/4_✔️_structure_to_spectrum.py
@@ -4,6 +4,10 @@ import numpy as np
 import pandas as pd
 
 import streamlit as st
+from streamlit_ketcher import st_ketcher
+
+from rdkit import Chem
+from rdkit.Chem import AllChem
 
 from api.SpectralPrediction.prediction_utils import xyz2data, predict_spectrum
 from api.SpectralPrediction.code.function import rdmol2pt
@@ -12,6 +16,12 @@ from utils.utils import generate_download_link
 
 cache_path = '/media/ramancloud/cache'
 
+
+def optmize_conformer(mol):
+    mol = Chem.AddHs(mol)
+    AllChem.EmbedMolecule(mol)
+    AllChem.MMFFOptimizeMolecule(mol)
+    return mol
 
 def upload_xyz_module(upload_file):
     time_cache = time.time()
@@ -36,7 +46,6 @@ def raman_predicting_module(rdmol, name):
 
 def demonstrate_mol(mol):
 
-    from rdkit import Chem
     import py3Dmol
     from stmol import showmol
 
@@ -55,27 +64,52 @@ def run():
     with st.container(border=True):
         st.session_state['mol'] = None
         st.subheader('Import data', divider='gray')
-        st.markdown('<font size=5>**Upload your xyz file**</font>',
-                    unsafe_allow_html=True)
-
-        upload_file = st.file_uploader(label=' ', accept_multiple_files=False, type=['xyz'],
-                                       label_visibility='collapsed')
-
-        if not upload_file:
-            st.markdown('<font size=5>**Or use demo data**</font>',
+        input_method = st.radio("Which method would you like to input the molecule?",
+                                ["**select one**:point_down:", "upload my .xyz file", "input the SMILES string", "draw the ideal molecule"],
+                                horizontal=False,)
+        if input_method == "**select one**:point_down:":
+            st.stop()
+        elif input_method == "input the SMILES string":
+            st.markdown('<font size=5>**Input the SMILES string**</font>',
                         unsafe_allow_html=True)
-            demo_data = st.selectbox(label=' ', label_visibility='collapsed', index=None,
-                                     options=['test'], placeholder='select demo data')
-
-            if demo_data == 'test':
-                xyz_file_path = '/media/ramancloud/samples/test.xyz'
-                demo_mol = xyz2data(xyz_file_path)
+            smile_code = st.text_input("SMILES", 'CCCO')
+            demo_mol = Chem.MolFromSmiles(smile_code)
+            demo_mol = optmize_conformer(demo_mol)
+            st.session_state['mol'] = demo_mol
+        elif input_method == "draw the ideal molecule":
+        
+            st.markdown('<font size=5>**Draw the molecule**</font>',
+                        unsafe_allow_html=True)
+            smile_code = st_ketcher(molecule_format='SMILES')
+            if not smile_code:
+                st.stop()
+            else:
+                demo_mol = Chem.MolFromSmiles(smile_code)
+                demo_mol = optmize_conformer(demo_mol)
                 st.session_state['mol'] = demo_mol
-        else:
-            demo_mol = upload_xyz_module(upload_file)
-            time.sleep(1)
-            st.warning(
-                'Here is our [user item and privacy policy.](privacy_policy)')
+
+        elif input_method == "upload my .xyz file":
+            st.markdown('<font size=5>**Upload your file**</font>',
+                        unsafe_allow_html=True)
+
+            upload_file = st.file_uploader(label=' ', accept_multiple_files=False, type=['xyz'],
+                                        label_visibility='collapsed')
+
+            if not upload_file:
+                st.markdown('<font size=5>**Or use demo data**</font>',
+                            unsafe_allow_html=True)
+                demo_data = st.selectbox(label=' ', label_visibility='collapsed', index=None,
+                                        options=['test'], placeholder='select demo data')
+
+                if demo_data == 'test':
+                    xyz_file_path = '/media/ramancloud/samples/test.xyz'
+                    demo_mol = xyz2data(xyz_file_path)
+                    st.session_state['mol'] = demo_mol
+            else:
+                demo_mol = upload_xyz_module(upload_file)
+                time.sleep(1)
+                st.warning(
+                    'Here is our [user item and privacy policy.](privacy_policy)')
 
     if 'mol' in st.session_state and st.session_state['mol'] is not None:
 
