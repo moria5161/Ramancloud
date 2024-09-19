8d05124fc862fc62aa508261f3c1b06789f2a366
xinyu lu <xinyulu@stu.xmu.edu.cn>
2024-09-19 11:02:22 +0800

    自动开启flask应用

--- 🏠_Homepage.py
+++ 🏠_Homepage.py
@@ -1,5 +1,7 @@
 from PIL import Image as Image
 import streamlit as st
+import subprocess
+import threading
 
 
 st.set_page_config(
@@ -302,3 +304,9 @@ if __name__ == "__main__":
     with col2:
         from streamlit.components.v1 import html
         html('''<a href='https://clustrmaps.com/site/1bxqy'  title='Visit tracker'><img src='//clustrmaps.com/map_v2.png?cl=ffffff&w=a&t=tt&d=IYobjN-Mu1pChSxslZv7Z5QG-hGiH_WbPUJNPPml1q0&co=2d78ad&ct=ffffff'/></a>''')
+
+    def run_flask():
+        subprocess.run(["flask", "--app", "/media/ramancloud_beta/multiprocess_flask.py", "run"])
+
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.start()
